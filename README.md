c96b3e31924769baf973e62abb769a1ed8e3a2c7
chairbender <kwhipke1@gmail.com>
2025-04-29 20:06:07 -0700

    getting started on "modern" kaleidoscope step 1

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Kaleidoscope LLVM Tutorial
 This is just my attempt at going through the LLVM Kaleidoscope tutorial in preparation for writing my
 own compiler: https://llvm.org/docs/tutorial/index.html
+
+However, on top of this tutorial, I'm trying to practice using the most up-to-date modern C++26 standards and best practices (at least those supported
+by Clang 20) and best practices, whereas the Kaleidoscope tutorial is rather old (but still super helpful for me!).
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,57 @@
 #include <iostream>
 
+using std::string, std::cin;
+
+enum class Token : uint8_t {
+    EndOfFile,
+    Def,
+    Extern,
+    Identifier,
+    Number
+};
+
+// TODO: do these really need to be globals?
+// filled in if Identifier
+static string TokenIdentifierStr;
+// filled in if NumVal
+static double TokenNumVal;
+
+// TODO: are these the best functions to use in modern C++?
+static Token gettok() {
+    static int LastChar = ' ';
+
+    // this skips whitespace automatically
+    // TODO: what happens on EOF?
+    cin >> LastChar;
+
+    // identifier: a-zA-Z0-9
+    if (isalpha(LastChar)) {
+        TokenIdentifierStr = static_cast<char>(LastChar);
+        while (isalnum(LastChar = cin.get()))
+            TokenIdentifierStr += static_cast<char>(LastChar);
+
+        if (TokenIdentifierStr == "def")
+            return Token::Identifier;
+        if (TokenIdentifierStr == "extern")
+            return Token::Extern;
+        return Token::Identifier;
+    }
+
+    // Number: 0-9
+    if (isdigit(LastChar) || LastChar == '.') {
+        string NumStr;
+        do {
+            NumStr += static_cast<char>(LastChar);
+            cin >> LastChar;
+        } while (isdigit(LastChar) || LastChar == '.');
+        TokenNumVal = stod(NumStr);
+        return Token::Number;
+    }
+
+    // TODO: LEFT OFF
+
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
